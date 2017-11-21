5c959956efb89c5b4ff5547d70f0d7ba33137a51
carlos eduardo <carlos.souza@cexgroup.com.br>
2017-11-21 17:59:59 -0200

    foto produto

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121150546) do
+ActiveRecord::Schema.define(version: 20171121193750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachinary_files", force: :cascade do |t|
+    t.string "attachinariable_type"
+    t.bigint "attachinariable_id"
+    t.string "scope"
+    t.string "public_id"
+    t.string "version"
+    t.integer "width"
+    t.integer "height"
+    t.string "format"
+    t.string "resource_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
+    t.index ["attachinariable_type", "attachinariable_id"], name: "attachinariable_index"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.string "delivery_address"
@@ -43,6 +59,7 @@ ActiveRecord::Schema.define(version: 20171121150546) do
     t.integer "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "stores", force: :cascade do |t|
