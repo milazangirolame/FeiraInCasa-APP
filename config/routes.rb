f44793f85cfd7f7c80852e09bd17096705b9f6e2
Mila Zangirolame <milazangirolame@gmail.com>
2017-11-21 10:53:11 -0200

    stores view

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-
  devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :stores
   resources :products
 
   resources :products
 
+  resources :products
 end
