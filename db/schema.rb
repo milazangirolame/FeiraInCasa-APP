9f4ee9162b7d7aee67431f957afb84503b828ff7
Mila Zangirolame <milazangirolame@gmail.com>
2017-11-21 15:03:09 -0200

    cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120174829) do
+ActiveRecord::Schema.define(version: 20171121150546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.string "delivery_address"
+    t.string "delivery_city"
+    t.string "delivery_zipcode"
+    t.date "delivery_date"
+    t.string "payment_method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "quantity"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_items_on_cart_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -71,5 +91,7 @@ ActiveRecord::Schema.define(version: 20171120174829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
+  add_foreign_key "items", "carts"
   add_foreign_key "stores", "users"
 end
