cbe0a34f2748664920e8c4ae7b16ca6b16a1ff37
Rodrigo Latache <rojlp2003@hotmail.com>
2017-11-23 10:18:09 -0200

    routes schema style gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
- devise_for :users,
   mount Attachinary::Engine => "/attachinary"
   devise_for :users,
-  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stores
