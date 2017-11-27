1e74ecec140991c16d2a38614049f6548947b212
Rodrigo Latache <rojlp2003@hotmail.com>
2017-11-27 15:00:43 -0200

    Cart parameters including Delivery Address,City, Zipcode and Payment Method were validated successfully

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   has_many :items
   has_many :products, through: :items
   belongs_to :user
+  validates :delivery_address, :delivery_city, :delivery_zipcode, :payment_method, presence: true
 end
