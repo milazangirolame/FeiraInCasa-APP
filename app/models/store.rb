ad634d5747ac790ad32a167b207f8228043eddf0
Rodrigo Latache <rojlp2003@hotmail.com>
2017-11-27 14:37:33 -0200

    Store and User parameters including address, nome da loja, user address and user first and last names were validated successfully using the presence true validation.

--- app/models/store.rb
+++ app/models/store.rb
@@ -4,6 +4,8 @@ class Store < ApplicationRecord
   serialize :days_of_week_opened, Array
   geocoded_by :full_address
   after_validation :geocode, if: :full_address_changed?
+  validates :name, :address, :city, :zipcode, presence: true
+
 
   def full_address
     "#{self.address}, #{self.city}, #{self.zipcode}"
