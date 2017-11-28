e2328cdad6146da1fa056de8ca7e04dada3f2885
Kaue Matteucci <kauefm@gmail.com>
2017-11-28 18:50:14 -0200

    Validações e apagar produto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,13 @@
 class Product < ApplicationRecord
   CATEGORIES = ["Legumes", "Frutas", "Bebidas" , "Verduras" , "Queijos" , "Ovos e Leite"]
+  acts_as_paranoid
   belongs_to :store
   has_many :items
-  has_attachments :photos, maximum: 10
+  has_attachments :photos, maximum: 1
   validates_inclusion_of :category, in: CATEGORIES
   validates :name, :category, :price, :unity, presence: true
+  validates :name, uniqueness: { scope: [:store_id]}
+  validates :photos, presence: true
 
   def item_for(user)
     user.current_cart.items.find_by(product: self) || Item.new(quantity: 0, product: self, cart: user.current_cart)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128144448) do
+ActiveRecord::Schema.define(version: 20171128203004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20171128144448) do
     t.integer "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_products_on_deleted_at"
   end
 
   create_table "stores", force: :cascade do |t|
