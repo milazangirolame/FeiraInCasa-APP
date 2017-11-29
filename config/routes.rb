4dc52209bd2bf73f0cfb6ab47c0f84a71998bbce
Rodrigo Latache <rojlp2003@hotmail.com>
2017-11-29 16:01:46 -0200

    Heading and Navbar translated successfully

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,18 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   mount Attachinary::Engine => "/attachinary"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stores
-  resources :products
-  resources :carts do
-    resources :items, only: [:create, :destroy, :update] do
-      collection do
-        post :more
-        post :less
+
+  scope '(:locale)', locale: /en/ do
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :stores
+    resources :products
+    resources :carts do
+      resources :items, only: [:create, :destroy, :update] do
+        collection do
+          post :more
+          post :less
+        end
       end
     end
   end
