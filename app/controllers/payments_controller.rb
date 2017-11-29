14077959d185fab903a55fbb1737c3fc63239d73
Kaue Matteucci <kauefm@gmail.com>
2017-11-29 18:22:20 -0200

    Feito

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,29 +1,30 @@
 class PaymentsController < ApplicationController
-  before_action :set_order
+  before_action :set_cart
 
   def new
 
   end
 
   def create
-  customer = Stripe::Customer.create(
-    source: params[:stripeToken],
-    email:  params[:stripeEmail]
-  )
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
 
-  charge = Stripe::Charge.create(
-    customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @cart.amount_centavos,
-    description:  "Pagamento feito com sucesso #{@cart.id}",
-    currency:     @cart.amount.currency
-  )
-  @cart.update(payment: charge.to_json, state: 'paid')
-  redirect_to stores_path
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @cart.amount_centavos,
+      description:  "Pagamento feito com sucesso #{@cart.id}",
+      currency:     @cart.amount.currency
+    )
+    @cart.update(payment: charge.to_json, state: 'paid')
+    flash[:notice] = "Pedido confirmado!"
+    redirect_to stores_path
 
 
-  rescue Stripe::CardError => e
-  flash[:alert] = e.message
-  redirect_to new_cart_payment_path(@cart)
+    rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to new_cart_payment_path(@cart)
   end
 
   def show
@@ -33,6 +34,6 @@ class PaymentsController < ApplicationController
 private
 
   def set_cart
-    @cart = Cart.where(state: 'pending').find(params[:cart_id])
+    @cart = Cart.find(params[:cart_id])
   end
 end
